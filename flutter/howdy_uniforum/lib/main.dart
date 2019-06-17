3e268aa5f4991efc9bffdc0e3795f5513f851259
kent archie <kentarchie@gmail.com>
2019-06-16 23:09:44 -0500

    small change to one app, added default app for example

--- flutter/howdy_uniforum/lib/main.dart
+++ flutter/howdy_uniforum/lib/main.dart
@@ -39,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String osName='RSTS';
   String maker='DEC';
   AndroidBuildVersion osVersion;
+
   Future getInfo() async {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
@@ -50,8 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // This method is rerun every time setState is called
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
@@ -72,39 +72,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
           //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'This is running on this OS:',
-            ),
-            Text(
-              osName,
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              'This is who made the device:',
-            ),
-            Text(
-              maker,
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              'This is the OS version:',
-            ),
-            Text(
-              osVersion.release,
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Text( 'This is running on this OS:', ),
+            Text( osName, style: Theme.of(context).textTheme.display1, ),
+            Text( 'This is who made the device:', ),
+            Text( maker, style: Theme.of(context).textTheme.display1, ),
+            Text( 'This is the OS version:', ),
+            Text( osVersion.release, style: Theme.of(context).textTheme.display1, ),
           ],
         ),
       ),
